89a9544230c74d54a88da25d6572d407346dbad2
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2020-06-26 15:21:14 +0200

    Initial commit

--- app-service-main.tf
+++ app-service-main.tf
@@ -19,12 +19,12 @@ resource "azurerm_app_service_plan" "service-plan" {
   name                = "kopi-${var.region}-${var.environment}-${var.app_name}-service-plan"
   location            = azurerm_resource_group.appservice-rg.location
   resource_group_name = azurerm_resource_group.appservice-rg.name
-  kind                = "Linux"
+  kind                = "Windows"
   reserved            = true
 
   sku {
-    tier = "Standard"
-    size = "S1"
+    tier = "PremiumV2"
+    size = "P1v2"
   }
 
   tags = {
@@ -42,7 +42,7 @@ resource "azurerm_app_service" "app-service" {
   app_service_plan_id = azurerm_app_service_plan.service-plan.id
 
   site_config {
-    linux_fx_version = "DOTNETCORE|3.1"
+    dotnet_framework_version = "v4.0"
   }
 
   tags = {
